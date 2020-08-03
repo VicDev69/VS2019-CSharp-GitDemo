64816d215b0fe26e15d067b761b1782d46eba5ca
Vic Graham <vic.graham@ntlworld.com>
2020-08-03 19:55:54 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS2019_CSharp_GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
